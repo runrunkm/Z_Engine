16090fd60b4a50598304a4b1dd82d83fe0bd28d0
runrunkm <191524547@qq.com>
2024-12-12 11:00:48 +0800

    First Commit

---
+++ PrimeEditor/GameProject/ProjectBrowserDialg.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrimeEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialg.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialg : Window
+    {
+        public ProjectBrowserDialg()
+        {
+            InitializeComponent();
+        }
+    }
+}
